8854fc60b13d1f71bcd4870eedced8aa40417896
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-12-25 14:30:47 -0500

    Create basename for RouteSwitch component

--- src/routeswitch.js
+++ src/routeswitch.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Navbar from "./components/navbar/index.js";
+import NewsCard from "./components/news-card";
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Navbar />
       <App />
       <Routes>
-        <Route path="/app" element={<App />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   );
